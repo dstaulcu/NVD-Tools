b283818058ebf5fa35ba5eda1a72822296f5ee2d
dstaulcu <dstaulcu@users.noreply.github.com>
2020-03-07 15:53:59 -0500

    Update Get-NVD-CPE-ProductInfo.ps1

--- Get-NVD-CPE-ProductInfo.ps1
+++ Get-NVD-CPE-ProductInfo.ps1
@@ -1,3 +1,4 @@
+
 function Get-Web-Download($url)
 {
 
@@ -29,11 +30,19 @@ function Expand-ZIPFile($file, $destination)
     }
 }
 
+# take note of start date-time
+$jobstart = Get-Date
+
 $nvd_cpe_url = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
 $nvd_cpe_filepath = "$env:TEMP\official-cpe-dictionary_v2.3.xml"
+$nvd_cpe_filepath_csv = "$env:TEMP\official-cpe-dictionary_v2.3.csv"
+
+# remove previons version of output csv file
+if (Test-Path -Path $nvd_cpe_filepath_csv) { 
+    Remove-Item -path $nvd_cpe_filepath_csv -Force
+}
 
 # remove previous version of downloads
-
 if (test-path $nvd_cpe_filepath) {
     Remove-Item $nvd_cpe_filepath -Force
 }
@@ -53,33 +62,64 @@ $xml = New-Object -TypeName XML
 $xml.Load($nvd_cpe_filepath)
 
 $records = @()
-foreach ($item in ($xml.'cpe-list'.'cpe-item')[0..1000]) {
+$counter = 0
+$pages = 0
+
+# loop through each cpe-item and append properties of interst to object
+foreach ($item in ($xml.'cpe-list'.'cpe-item')) {
+
+    # increment counter of items observed
+    $counter++
+
+    # extract vendor, product, and version from name field (better, but not best, for humans)
     $item.name -match "cpe\:/a:([^\:]+):([^\:]+):([^\:]+)" | Out-Null
     if ($matches) { 
         $vendor = $matches[1]
         $product = $matches[2]
         $version = $matches[3]    
-    } else { $vendor = "failed" ; $product = "failed" ; $version = "failed" }
+    } else { $vendor = "extract_failed" ; $product = "extract_failed" ; $version = "extract_failed" }
 
-    $vendormatch = $false ; $productmatch = $false ; $versionmatch = $false
-    if ($title -match $vendor) { $vendormatch = $true}
-    if ($title -match $product) { $productmatch = $true}
-    if ($title -match $version) { $versionmatch = $true}        
-
+    # tee up a hash table of properties of interest
     $Record = @{
         name =  $item.name
         vendor = $vendor
         product = $product
         version = $version     
-        title = ($item.title | ?{$_.lang -eq 'en-US'}).'#text'
+        title = ($item.title | where-object{$_.lang -eq 'en-US'}).'#text'
     }
 
-    # really just looking to point out differences in cpe name vs. cpe title
-    if ($vendormatch -eq $false -or $productmatch -eq $false -or $versionmatch -eq $false) {
-        $Records += New-Object -TypeName PSObject -Property $Record
+    # append records array with hash table members
+    $Records += New-Object -TypeName PSObject -Property $Record
+
+    # now, if we are at 1000 items, lets page out to CSV files
+    if ($counter -eq 10000) { 
+            $pages++
+            $counter = 0
+            write-host "$(get-date) - Appending results file $($nvd_cpe_filepath_csv) with 10K processed records. `[Page $($pages)`]."
+            $records | Export-Csv -Path $nvd_cpe_filepath_csv -NoTypeInformation -Append
+            # reset the records object
+            $records = @() 
     }
-
-
 } 
 
+# now that we are done with the loop, append remaining content to CSV file
+write-host "$(get-date) - Appending results file $($nvd_cpe_filepath_csv) with remaining $($counter) records."
+$records | Export-Csv -Path $nvd_cpe_filepath_csv -NoTypeInformation -Append
+
+# give back that memory
+$xml | Out-Null
+
+# prepare results for interaction
+$Records = Import-Csv -Path $nvd_cpe_filepath_csv
+
+# write summary of time to execute
+$jobTotalSeconds = (New-TimeSpan -Start $jobstart).TotalSeconds
+
+write-host "Task completed in $($jobTotalSeconds) seconds producting $($records.count) records!"
+
+<#
+$records | Select-Object -Property name, title, vendor, product, version | out-GridView    
+$Records = Import-Csv -Path $nvd_cpe_filepath_csv
+$records | ?{$_.name -match "[^~]~$"} | Select-Object -Property name, title, vendor, product, version | out-GridView    
 $records | Select-Object -Property name, title, vendor, product, version | Sort-Object -Property name | Out-GridView
+#>
