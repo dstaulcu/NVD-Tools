b67919401713cd77d1beb4783f3bfdcea003e998
dstaulcu <dstaulcu@users.noreply.github.com>
2020-03-09 00:04:16 -0400

    Add files via upload

--- Get-NVD-CPE-ProductInfo.ps1
+++ Get-NVD-CPE-ProductInfo.ps1
@@ -1,3 +1,7 @@
+$nvd_feed_url = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
+$nvd_feed_filepath = "$env:TEMP\official-cpe-dictionary_v2.3.xml"
+$nvd_feed_filepath_csv = "$env:TEMP\official-cpe-dictionary_v2.3.csv"
+$page_limit = 10000
 
 function Get-Web-Download($url)
 {
@@ -32,32 +36,28 @@ function Expand-ZIPFile($file, $destination)
 # take note of start date-time
 $jobstart = Get-Date
 
-$nvd_cpe_url = "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
-$nvd_cpe_filepath = "$env:TEMP\official-cpe-dictionary_v2.3.xml"
-$nvd_cpe_filepath_csv = "$env:TEMP\official-cpe-dictionary_v2.3.csv"
-
 # remove previons version of output csv file
-if (Test-Path -Path $nvd_cpe_filepath_csv) { 
-    Remove-Item -path $nvd_cpe_filepath_csv -Force
+if (Test-Path -Path $nvd_feed_filepath_csv) { 
+    Remove-Item -path $nvd_feed_filepath_csv -Force
 }
 
 # remove previous version of downloads
-if (test-path $nvd_cpe_filepath) {
-    Remove-Item $nvd_cpe_filepath -Force
+if (test-path $nvd_feed_filepath) {
+    Remove-Item $nvd_feed_filepath -Force
 }
 
 # download most recent zip CVE list
-Write-host "$(get-date) - Downloading $($nvd_cpe_url)."
-$download_path = Get-Web-Download -url $nvd_cpe_url
+Write-host "$(get-date) - Downloading $($nvd_feed_url)."
+$download_path = Get-Web-Download -url $nvd_feed_url
 
 # extract the zipped CVE list
 Write-host "$(get-date) - Expanding $($download_path)."
 Expand-ZIPFile -file $download_path -destination $env:TEMP
 
 # read entries into an xml
-Write-host "$(get-date) - Reading $($nvd_cpe_filepath) into xml object"
+Write-host "$(get-date) - Reading $($nvd_feed_filepath) into xml object"
 $xml = New-Object -TypeName XML
-$xml.Load($nvd_cpe_filepath)
+$xml.Load($nvd_feed_filepath)
 
 # convert xml entries of interest into csv
 Write-host "$(get-date) - Convert xml entries of interest to CSV"
@@ -81,7 +81,7 @@ foreach ($item in ($xml.'cpe-list'.'cpe-item')[0..2000]) {   # remove slicing wh
         $version = $matches[3]    
     } else { $vendor = "extract_failed" ; $product = "extract_failed" ; $version = "extract_failed" }
 
-
+    # extract the url of of some of the more common references (change log, version)
     $references = @($item.references.ChildNodes)
     $changelog_href = ($references | where-object{$_.'#text' -eq "Change Log"}).href | Select-Object -First 1  
     $version_href = ($references | where-object{$_.'#text' -eq "Version"}).href | Select-Object -First 1
@@ -102,22 +102,24 @@ foreach ($item in ($xml.'cpe-list'.'cpe-item')[0..2000]) {   # remove slicing wh
     $Records += New-Object -TypeName PSObject -Property $Record
 
     # now, if we are at N items, lets page out to CSV files
-    if ($page_size -eq 10000) { 
+    if ($page_size -eq $page_limit) { 
             $page_count++
             $page_size = 0
-            write-host "$(get-date) - Appending results file $($nvd_cpe_filepath_csv) with 10K processed records. `[Page $($page_count)`]."
-            $records | Export-Csv -Path $nvd_cpe_filepath_csv -NoTypeInformation -Append
+            write-host "$(get-date) - Appending results file $($nvd_feed_filepath_csv) with $($page_limit) processed records. `[Page $($page_count)`]."
+            $records | Export-Csv -Path $nvd_feed_filepath_csv -NoTypeInformation -Append
             # reset the records object
             $records = @() 
     }
 } 
 
 # now that we are done with the loop, append remaining content to CSV file
-write-host "$(get-date) - Appending results file $($nvd_cpe_filepath_csv) with remaining $($counter) records."
-$records | Export-Csv -Path $nvd_cpe_filepath_csv -NoTypeInformation -Append
+if ($records.count -ge 1) {  
+    write-host "$(get-date) - Appending results file $($nvd_feed_filepath_csv) with remaining $($page_size) records."
+    $records | Export-Csv -Path $nvd_feed_filepath_csv -NoTypeInformation -Append
+}
 
 # give back that memory
-#$xml | Out-Null
+$xml | Out-Null
 
 # write summary of time to execute
 $jobTotalSeconds = (New-TimeSpan -Start $jobstart).TotalSeconds
@@ -131,7 +133,7 @@ write-host "$(get-date) - Task completed in $($jobTotalSeconds) seconds converti
 ###################################
 
 # read stored records into powershell PSCustomObject
-$Records = Import-Csv -Path $nvd_cpe_filepath_csv
+$Records = Import-Csv -Path $nvd_feed_filepath_csv
 
 # show records 
 $records | Select-Object -Property name, cpe23item_name, title, vendor, product, version, changelog_url, version_url
